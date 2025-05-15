81e6743dacdb2e51aaa565065982fc53c6696373
Damian Chavez <damianfinance1@gmail.com>
2025-05-14 19:32:45 -0600

    actualizacion NumeroSecreto.java

--- src/NumeroSecreto.java
+++ src/NumeroSecreto.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class NumeroSecreto {
 
     public static void main(String[] args) {
         int numeroSecreto = new Random().nextInt(100);
@@ -9,12 +9,26 @@ public class Main {
         System.out.println(numeroSecreto);
         int contador = 0;
         int intentosUsuario = 0;
-        
-        while (numeroUsuario != numeroSecreto){
+
+        while (numeroUsuario != numeroSecreto) {
             Scanner teclado = new Scanner(System.in);
             System.out.println("dijita un numero entre uno y 100");
-            teclado.nextInt();
+            intentosUsuario++;
+            numeroUsuario = teclado.nextInt();
 
+            if (numeroUsuario == numeroSecreto) {
+                System.out.println("Felicidades acertaste el numero secreto a la :" + intentosUsuario );
+                } else if (numeroUsuario > numeroSecreto) {
+                System.out.println("El numero secreto es menor");
+                }else {
+                System.out.println("El numero secreto es mayor");
+
+                contador ++;
+                if (contador == 5 ){
+                    System.out.println("llegaste al numero maximo de intentos");
+                    break;
+                }
+            }
         }
     }
 }
